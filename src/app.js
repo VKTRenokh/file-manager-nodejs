0c211895383d0dfaeacfa138a71ef570abf53ac9
VKTRenokh <viktorenokh@gmail.com>
2023-06-22 16:01:00 +0300

    feat: add user class

--- src/app.js
+++ src/app.js
@@ -1,23 +1,10 @@
 import { FileManager } from "./file-manager.js";
+import { User } from "./user/user.js";
 
 export class App {
   constructor() {
-    const args = process.argv.slice(2);
-
-    this.fileManager = new FileManager(this.getName(args));
-  }
-
-  getName(args) {
-    const arg = args.find((arg) => {
-      return arg.startsWith("--username");
-    });
-
-    if (!arg) {
-      console.log("no username was given");
-      return;
-    }
-
-    return arg.slice(arg.indexOf("=") + 1);
+    this.user = new User()
+    this.fileManager = new FileManager(this.user.name);
   }
 }
 
